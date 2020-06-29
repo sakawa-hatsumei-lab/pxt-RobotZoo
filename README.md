08f207671c3e55d542a4a65483832851ea69ad48
eggplaNS <eggplaNS@users.noreply.github.com>
2020-06-29 15:01:43 +0900

    localization / ローカライゼーション

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # pxt-RobotZoo（WIP）
 micro:bit extension for "Robot Zoo" from Sakawa.
+https://hatsumei-lab.org/concierge/robot-zoo/
 
 ## LICENSE
-MIT
+MIT License
 
 ## Supported targets
 * for PXT/microbit
--- samples.ts
+++ samples.ts
@@ -1,29 +1,30 @@
 //% weight=200 color=#ff8308 icon=""
+//% block="ロボット動物園"
 //% groups="['かんさつ','つくる']"
 //かんさつ
-namespace ロボット動物園 {
+namespace RobotZoo {
     /**
      * 動きサンプル1 : 
      * 左右交互に少しずつ前に進む
      */
-    //% block
+    //% block="ちょこちょこ" block.loc.ja="ちょこちょこ"
     //% group="かんさつ"
     //% weight=600
-    export function _ちょこちょこ() {
+    export function toddle() {
         for(let i = 0; i < 4; i++) {
             pins.servoWritePin(AnalogPin.P15, 48);
             pins.servoWritePin(AnalogPin.P16, 90);
-            //ロボット動物園.めをあける();
+            // RobotZoo.openEyes();
             basic.pause(200);
             pins.servoWritePin(AnalogPin.P15, 90);
             pins.servoWritePin(AnalogPin.P16, 132);
-            //ロボット動物園.めをあける();
+            // RobotZoo.openEyes();
             basic.pause(200);
             pins.servoWritePin(AnalogPin.P15, 90);
             pins.servoWritePin(AnalogPin.P16, 90);
             pins.analogWritePin(AnalogPin.P15, 0);
             pins.analogWritePin(AnalogPin.P16, 0);
-            //ロボット動物園.めをあける();
+            // RobotZoo.openEyes();
         }
     }
 
@@ -31,25 +32,25 @@ namespace ロボット動物園 {
      * 動きサンプル2 : 
      * 緩やかにカーブしながら前に進む
      */
-    //% block
+    //% block="すいすい"
     //% group="かんさつ"
     //% weight=500
     //% block=""
-    export function _すいすい() {
+    export function swift() {
         for(let i = 0; i < 2; i++) {
             pins.servoWritePin(AnalogPin.P15, 40);
             pins.servoWritePin(AnalogPin.P16, 100);
-            //ロボット動物園.めをあける();
+            // RobotZoo.openEyes();
             basic.pause(700);
             pins.servoWritePin(AnalogPin.P15, 80);
             pins.servoWritePin(AnalogPin.P16, 130);
-            //ロボット動物園.めをあける();
+            // RobotZoo.openEyes();
             basic.pause(700);
             pins.servoWritePin(AnalogPin.P15, 90);
             pins.servoWritePin(AnalogPin.P16, 90);
             pins.analogWritePin(AnalogPin.P15, 0);
             pins.analogWritePin(AnalogPin.P16, 0);
-            //ロボット動物園.めをあける();
+            // RobotZoo.openEyes();
         }
     }
     
@@ -58,20 +59,20 @@ namespace ロボット動物園 {
      * 動きサンプル3 : 
      * ちょっと進んでちょっと止まる
      */
-    //% block
+    //% block="つんつん"
     //% group="かんさつ"
     //% weight=400
-    export function _つんつん() {
+    export function poke() {
         for(let i = 0; i < 8; i++) {
             pins.servoWritePin(AnalogPin.P15, 68);
             pins.servoWritePin(AnalogPin.P16, 112);
-            //ロボット動物園.めをあける();
+            // RobotZoo.openEyes();
             basic.pause(300);
             pins.servoWritePin(AnalogPin.P15, 90);
             pins.servoWritePin(AnalogPin.P16, 90);
             pins.analogWritePin(AnalogPin.P15, 0);
             pins.analogWritePin(AnalogPin.P16, 0);
-            //ロボット動物園.めをあける();
+            // RobotZoo.openEyes();
             basic.pause(150);
         }
     }
@@ -80,52 +81,52 @@ namespace ロボット動物園 {
      * 動きサンプル4 : 
      * 前に進んでどこかを向き、また前に進む
      */
-    //% block
+    //% block="うろうろ"
     //% group="かんさつ"
     //% weight=300
-    export function _うろうろ() {
+    export function stroll() {
         pins.servoWritePin(AnalogPin.P15, 68);
         pins.servoWritePin(AnalogPin.P16, 112);
-        //ロボット動物園.めをあける();
+        // RobotZoo.openEyes();
         basic.pause(500);
-        //pins.servoWritePin(AnalogPin.P15, 90);
-        //pins.servoWritePin(AnalogPin.P16, 90);
+        // pins.servoWritePin(AnalogPin.P15, 90);
+        // pins.servoWritePin(AnalogPin.P16, 90);
         pins.analogWritePin(AnalogPin.P15, 0);
         pins.analogWritePin(AnalogPin.P16, 0);
-        //ロボット動物園.めをあける();
+        // RobotZoo.openEyes();
         basic.pause(700);
 
         pins.servoWritePin(AnalogPin.P15, 48);
         pins.servoWritePin(AnalogPin.P16, 48);
-        //ロボット動物園.めをあける();
+        // RobotZoo.openEyes();
         basic.pause(500);
-        //pins.servoWritePin(AnalogPin.P15, 90);
-        //pins.servoWritePin(AnalogPin.P16, 90);
+        // pins.servoWritePin(AnalogPin.P15, 90);
+        // pins.servoWritePin(AnalogPin.P16, 90);
         pins.analogWritePin(AnalogPin.P15, 0);
         pins.analogWritePin(AnalogPin.P16, 0);
-        //ロボット動物園.めをあける();
+        // RobotZoo.openEyes();
         basic.pause(100);
 
         pins.servoWritePin(AnalogPin.P15, 48);
         pins.servoWritePin(AnalogPin.P16, 132);
-        //ロボット動物園.めをあける();
+        // RobotZoo.openEyes();
         basic.pause(700);
-        //pins.servoWritePin(AnalogPin.P15, 90);
-        //pins.servoWritePin(AnalogPin.P16, 90);
+        // pins.servoWritePin(AnalogPin.P15, 90);
+        // pins.servoWritePin(AnalogPin.P16, 90);
         pins.analogWritePin(AnalogPin.P15, 0);
         pins.analogWritePin(AnalogPin.P16, 0);
-        //ロボット動物園.めをあける();
+        // RobotZoo.openEyes();
         basic.pause(700);
 
         pins.servoWritePin(AnalogPin.P15, 132);
         pins.servoWritePin(AnalogPin.P16, 132);
-        //ロボット動物園.めをあける();
+        // RobotZoo.openEyes();
         basic.pause(800);
         pins.servoWritePin(AnalogPin.P15, 90);
         pins.servoWritePin(AnalogPin.P16, 90);
         pins.analogWritePin(AnalogPin.P15, 0);
         pins.analogWritePin(AnalogPin.P16, 0);
-        //ロボット動物園.めをあける();
+        // RobotZoo.openEyes();
         basic.pause(300);
     }
     
@@ -133,47 +134,47 @@ namespace ロボット動物園 {
      * 動きサンプル5 : 
      * 少し進んであたりを見る
      */
-    //% block
+    //% block="きょろきょろ"
     //% group="かんさつ"
     //% weight=200
-    export function _きょろきょろ() {
+    export function lookAlound() {
         for(let i = 0; i < 3; i++) {
             pins.servoWritePin(AnalogPin.P15, 58);
             pins.servoWritePin(AnalogPin.P16, 122);
-            //ロボット動物園.めをあける();
+            // RobotZoo.openEyes();
             basic.pause(500);
 
             pins.servoWritePin(AnalogPin.P15, 48);
             pins.servoWritePin(AnalogPin.P16, 48);
-            //ロボット動物園.めをあける();
+            // RobotZoo.openEyes();
             basic.pause(150);
-            //pins.servoWritePin(AnalogPin.P15, 90);
-            //pins.servoWritePin(AnalogPin.P16, 90);
+            // pins.servoWritePin(AnalogPin.P15, 90);
+            // pins.servoWritePin(AnalogPin.P16, 90);
             pins.analogWritePin(AnalogPin.P15, 0);
             pins.analogWritePin(AnalogPin.P16, 0);
-            //ロボット動物園.めをあける();
+            // RobotZoo.openEyes();
             basic.pause(500);
 
             pins.servoWritePin(AnalogPin.P15, 132);
             pins.servoWritePin(AnalogPin.P16, 132);
-            //ロボット動物園.めをあける();
+            // RobotZoo.openEyes();
             basic.pause(300);
-            //pins.servoWritePin(AnalogPin.P15, 90);
-            //pins.servoWritePin(AnalogPin.P16, 90);
+            // pins.servoWritePin(AnalogPin.P15, 90);
+            // pins.servoWritePin(AnalogPin.P16, 90);
             pins.analogWritePin(AnalogPin.P15, 0);
             pins.analogWritePin(AnalogPin.P16, 0);
-            //ロボット動物園.めをあける();
+            // RobotZoo.openEyes();
             basic.pause(500);
 
             pins.servoWritePin(AnalogPin.P15, 48);
             pins.servoWritePin(AnalogPin.P16, 48);
-            //ロボット動物園.めをあける();
+            // RobotZoo.openEyes();
             basic.pause(150);
             pins.servoWritePin(AnalogPin.P15, 90);
             pins.servoWritePin(AnalogPin.P16, 90);
             pins.analogWritePin(AnalogPin.P15, 0);
             pins.analogWritePin(AnalogPin.P16, 0);
-            //ロボット動物園.めをあける();
+            // RobotZoo.openEyes();
             basic.pause(300);
         }
     }
@@ -182,13 +183,13 @@ namespace ロボット動物園 {
      * 動きサンプル6 : 
      * 勢いよくまっすぐ前に進む
      */
-    //% block
+    //% block="ぐんぐん"
     //% group="かんさつ"
     //% weight=100
-    export function _ぐんぐん() {
+    export function rush() {
         pins.servoWritePin(AnalogPin.P15, 0);
         pins.servoWritePin(AnalogPin.P16, 180);
-        //ロボット動物園.めをあける();
+        // RobotZoo.openEyes();
         basic.pause(700);
     }
 }
